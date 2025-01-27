74d4245d61c922c63db21871089c8ddbb2015e4e
Leon Ong <ongl@oregonstate.edu>
2025-01-26 17:57:06 -0800

    FINAL FRONTEND

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import styled from '@emotion/styled';
-import './index.css'
-
 
 const Nav = styled.nav`
     background-color: rgb(33 37 41);
     padding-bottom: 10px;
     margin-bottom: 40px;
     display: flex;
-    justify-content: center;
+    justify-content: space-evenly;
     padding-top: 10px;
     border-top: 1px solid grey;
     border-bottom: 1px solid grey;
-
-
 `;
 
 const NavItem = styled.div`
@@ -22,13 +18,11 @@ const NavItem = styled.div`
 `;
 
 const Container = styled.div`
-    // background-color: rgb(100, 95, 95); /* Same color as navbar */
     text-align: center; /* Center-align the header */
     padding: 5px 0; /* Add padding around the header */
 `;
 
 const Heading = styled.h1`
-    // background-color: rgb(100, 95, 95); /* Same color as navbar */
     padding: 20px 0; /* Add padding to the heading for better spacing */
     color: #d73f09; /* Set text color to orange */
 `;
@@ -38,15 +32,32 @@ const Wrapper = styled.div`
 `;
 
 const Footer = styled.footer`
-    bottom: 0px;
+    margin: 0px;
     position: fixed;
+    bottom: 0px;
     display: flex;
-    justify-content: center;
-    align-content: center;
-    width: 100%;
-    padding: 13px;
-    padding-left: 0px;
-    background-color: rgb(24, 22, 22);;
+    justify-content: space-between;
+    align-items: center;
+    width: calc(100% - 20px); /* Adjust width to prevent overlap with scrollbar */
+    padding: 5px 20px; /* Reduce padding to make the footer smaller */
+    background-color: rgb(24, 22, 22);
+`;
+
+const FootTitle = styled.h2`
+    color: #d73f09;
+sldaslasla`;
+
+const FooterLink = styled.a`
+    color: #d73f09;
+    text-decoration: none;
+    margin: 0 15px;
+    &:hover {
+        text-decoration: underline;
+    }
+`;
+
+const MainContent = styled.div`
+    margin-bottom: 100px; /* Increase margin to ensure sufficient space above footer */
 `;
 
 function Root() {
@@ -63,10 +74,15 @@ function Root() {
                     <NavItem><NavLink to="/recommendation" className="nav-link">Recommendations</NavLink></NavItem>
                 </Nav>
             </Wrapper>
-            <div><Outlet/></div>    
-            <Footer >
-                The Archive Project
-                {/* Spotify Data Powered by: <a href="https://developer.spotify.com/documentation/web-api"> developer.spotify.com</a> */}
+            <MainContent>
+                <Outlet />
+            </MainContent>    
+            <Footer>
+                <FootTitle>The Archive Project</FootTitle>
+                <div>
+                    <FooterLink href="https://media.oregonstate.edu/tag?tagid=oh53" target="_blank" rel="noopener noreferrer">The Archive Site</FooterLink>
+                    <FooterLink href="https://scarc.library.oregonstate.edu/oralhistory.html" target="_blank" rel="noopener noreferrer">SCARC OSU</FooterLink>
+                </div>
             </Footer>
         </>
     );
